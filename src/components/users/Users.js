38823af941652ccc9e033a0b5bf569019c41ad28
Ernesto Samaniego <esamaniego53@gmail.com>
2019-09-15 02:51:56 -0700

    added axios to get requests

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -3,26 +3,8 @@ import UserItem from "./UserItem";
 
 export class Users extends Component {
   state = {
-    users: [
-      {
-        id: "1",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        id: "2",
-        login: "Sif",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        id: "3",
-        login: "Sifuentes",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      }
-    ]
+    users: [],
+    loading: false
   };
 
   render() {
